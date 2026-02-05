2cb1ae2971adc5131625a2a9d672143a62b4a81d
Cherry6628 <cherrycherry6628@gmail.com>
2026-02-05 11:04:52 +0530

    Password Manager bug fixed

--- JS/Extensions/passwordManager/content.js
+++ JS/Extensions/passwordManager/content.js
@@ -1,73 +1,27 @@
-console.log("Hello From Password Manager");
-
-let multipleTimes = false;
-function savePassword(app, mail, pwd) {
-    browser.runtime
-        .sendMessage({
-            action: "SAVE_PASSWORD",
-            data: { app, mail, pwd },
-        })
-        .then((r) => {
-            console.log(r);
-        });
-}
-function t(a) {
-    // debugger function
-    console.log(a);
-}
-
-const observer = new MutationObserver(() => {
-    t(0);
-    t(1);
-    if (multipleTimes) return;
-    t(2);
+(function () {
+    function savePassword(app, mail, pwd) {
+        browser.runtime
+            .sendMessage({ action: "SAVE_PASSWORD", data: { app, mail, pwd } })
+            .then((r) => console.log(r));
+    }
+    let cachemail = "", cachepwd = "";
+    function updateCache() {
+        cachemail = mailinp.value;
+        cachepwd = pwdinp.value;
+    }
     const appinp = location.hostname;
     const mailinp = document.querySelector("[type=email],[name~=username]");
     const pwdinp = document.querySelector("[type=password]");
-    let cachedMail = "";
-    let cachedPwd = "";
 
-    console.log(pwdinp, pwdinp.value, mailinp, mailinp.value);
-    if (!pwdinp || !pwdinp.value || !mailinp) return;
-    // console.log()
-    t(3);
-    multipleTimes = true;
-    t(4);
-    observer.disconnect();
-    t(5);
-    mailinp.addEventListener("input", () => {
-        cachedMail = mailinp.value;
-    });
-
-    pwdinp.addEventListener("input", () => {
-        cachedPwd = pwdinp.value;
-    });
-
+    mailinp?.addEventListener("change",updateCache);
+    pwdinp?.addEventListener("change",updateCache);
     document.addEventListener("click", (e) => {
-        t(6);
-        console.log("clicked");
-        let target = e.target;
-        console.log(target);
-        window.temp =
-            target.closest("[type=submit]") || target.closest("button");
-
-        target = window.temp;
-        delete window.temp;
-        console.log(target);
+        let target =
+            e.target.closest("[type=submit]") || e.target.closest("button");
         if (!target) return;
+        if (!cachemail || !cachepwd) return;
 
-        let mailv = cachedMail;
-        let pwdv = cachedPwd;
-
-        console.log(mailv, pwdv);
-        if (!confirm("DO YOU WANT TO SAVE THE PASSWORD ? ")) return;
-        savePassword(appinp, mailv, pwdv);
-        // let uname = mailinp.value;
+        confirm("DO YOU WANT TO SAVE THE PASSWORD ? ") &&
+            savePassword(appinp, cachemail, cachepwd);
     });
-});
-
-observer.observe(document.body, {
-    childList: true,
-    subtree: true,
-    attributes: true,
-});
+})();
