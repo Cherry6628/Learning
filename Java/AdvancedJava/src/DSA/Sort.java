d5c137fe49f1e83ffe23dfe792d2df49215b16c0
Cherry6628 <cherrycherry6628@gmail.com>
2026-02-06 06:54:34 +0530

    Sort.java file updated

--- Java/AdvancedJava/src/DSA/Sort.java
+++ Java/AdvancedJava/src/DSA/Sort.java
@@ -23,16 +23,6 @@ public class Sort {
 				else break;
 			}
 		}
-		
-//		for (int i=0;i<arr.length-1;i++) {
-//			for (int j=arr.length-1;j>0;j--) {
-//				if (arr[j]<arr[j-1]) {
-//					int t=arr[j-1];
-//					arr[j-1]=arr[j];
-//					arr[j]=t;
-//				}
-//			}
-//		}
 	}
 	
 	public static int[] merge(int[]a1, int[]a2) {
@@ -40,20 +30,25 @@ public class Sort {
 		int p0=0;
 		int p1=0;
 		int p2=0;
-		while(p0<a.length) {
-			if(p1<a1.length&&p2<a2.length&&a1[p1]<a2[p2]) {
+		while(p1<a1.length&&p2<a2.length) {
+			if(a1[p1]<a2[p2]) {
 				a[p0]=a1[p1];
 				p1++;p0++;
 			}
-			else if (p2<a2.length){
+			else{
 				a[p0]=a2[p2];
 				p2++;p0++;
 			}
-			System.out.println(Arrays.toString(a));
 		}
-		System.out.println("1");
-		System.out.println("\u00A0");
-		System.out.println("1");
+		while(p1<a1.length) {
+			a[p0]=a1[p1];
+			p1++;p0++;
+		}
+		while(p2<a2.length) {
+			a[p0]=a2[p2];
+			p2++;p0++;
+		}
+
 		return a;
 	}
 	
